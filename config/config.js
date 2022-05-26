d5420aa95cabc097b49c8856f211f4b6d6d4d5ad
Christia Narváez <saee1994@MacBook-Pro-de-Christian.local>
2022-05-25 19:06:14 -0500

    change database

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@ const env = process.env;
 
 const config = {
   db: { 
-    host: env.DB_HOST || 'ec2-52-5-110-35.compute-1.amazonaws.com',
+    host: env.DB_HOST || 'ec2-34-227-120-79.compute-1.amazonaws.com',
     port: env.DB_PORT || '5432',
-    user: env.DB_USER || 'ppzfdhzwdxhrgn',
-    password: env.DB_PASSWORD || '391cb814d46724c2832da4e3a27a03d3d8e437eb7ec00236558e5c4258d8c45c',
-    database: env.DB_NAME || 'db599oucbhfgfp',
+    user: env.DB_USER || 'qrgrxqcdwjvbnr',
+    password: env.DB_PASSWORD || '21f23948552e65cf6215cf20d060f38420f047494ac45a6beb955b6c164efc69',
+    database: env.DB_NAME || 'de6v6tjb87f00s',
     ssl: {
         rejectUnauthorized: false
     }
